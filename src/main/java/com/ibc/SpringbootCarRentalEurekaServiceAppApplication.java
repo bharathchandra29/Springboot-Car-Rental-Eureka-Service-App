0c926ca2f45d7d454d283eea994bf76368a6833f
bharathchandra29 <bharathchandra.ila@gmail.com>
2024-08-28 18:07:48 +0530

    Files added

---
+++ src/main/java/com/ibc/SpringbootCarRentalEurekaServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.ibc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCarRentalEurekaServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCarRentalEurekaServiceAppApplication.class, args);
+	}
+
+}
